57f631eb11e1f1293f10779a9f34ff27f772add3
moggst74 <60689573+moggst74@users.noreply.github.com>
2020-07-19 19:37:41 +1000

    Add files via upload

---
+++ 6. Neural Networks/2. Image Classifier Part 2/1. Work In Progress/predict.py
@@ -0,0 +1,134 @@
+# PROGRAMMER: Steve M 
+# DATE CREATED: 19.07.20                                 
+# REVISED DATE: 
+# PURPOSE: Load model checkpoint and rebuild model without having to retrain
+
+# Import necessary libraries
+# Import necessary libraries
+import matplotlib.pyplot as plt
+import torch
+import time
+import numpy as np
+from torchvision import datasets, transforms, models
+from torch import nn
+from torch import optim
+from collections import OrderedDict
+import torch.nn.functional as F
+import torchvision.models as models
+from torch.autograd import Variable
+from PIL import Image
+import seaborn as sns
+import argparse
+import json
+
+# Imports functions created for this program
+from get_input_args_predict import get_input_args
+
+def main():
+
+    # Function to retrieve command line arguments entered by the user
+    in_arg = get_input_args()
+    
+    #Load pre-trained model from checkpoint
+    model = load_checkpoint(in_arg.checkpoint)
+    #print(model)
+    
+    # Process Image
+    processed_image = process_image(in_arg.image_path) 
+    
+    # Classify Prediction
+    top_probs, top_labels, top_flowers = predict(in_arg.image_path, model, in_arg.category_names, in_arg.top_k)
+    
+###############################    
+def load_checkpoint(filepath):   
+#   device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    checkpoint = torch.load(filepath)
+
+    model = models.vgg19(pretrained=True)
+    
+    for param in model.parameters():
+        param.requires_grad = False
+    
+    model.class_to_idx = checkpoint['class_to_idx']
+    
+    # Hyperparameters for our network
+    input_size = 25088
+    hidden_sizes = [4096, 256]
+    output_size = 102
+    
+    # Create the classifier
+    classifier = nn.Sequential(OrderedDict([
+                                          ('fc1', nn.Linear(input_size, hidden_sizes[0],  bias=True)), #first layer
+                                          ('Relu1', nn.ReLU()), #apply activation function
+                                          ('Dropout1', nn.Dropout(p = 0.3)),
+                                          ('fc2', nn.Linear(hidden_sizes[0], output_size, bias=True)), #output layer
+                                          ('output', nn.LogSoftmax(dim=1)) #apply loss function
+                                          ]))
+    
+    # Put the classifier on the pretrained network
+    model.classifier = checkpoint['classifier']
+    model.load_state_dict(checkpoint['state_dict'])
+    #model.arch = checkpoint['arch'] 
+            
+    return model
+
+###############################
+def process_image(image_path):
+    ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
+        returns an Numpy array
+    '''
+    
+    # TODO: Process a PIL image for use in a PyTorch model
+    # Use same image transformation code as earlier
+    img = Image.open(image_path)
+    transform_image = transforms.Compose([transforms.Resize(256),
+                                      transforms.CenterCrop(224),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize([0.485, 0.456, 0.406], 
+                                                           [0.229, 0.224, 0.225])])
+    image_tensor = transform_image(img)
+    
+    return image_tensor
+
+###############################
+
+def predict(image_path, model, category_names, topk):
+    #Predict the class (or classes) of an image using a trained deep learning model
+    
+    # TODO: Implement the code to predict the class from an image file   
+    processed_image = process_image(image_path)
+    processed_image.unsqueeze_(0)
+    probs = torch.exp(model.forward(processed_image))
+    top_probs, top_labs = probs.topk(topk)
+
+    with open(category_names, 'r') as f:
+        cat_to_name = json.load(f)
+        
+    idx_to_class = {}
+    for key, value in model.class_to_idx.items():
+        idx_to_class[value] = key
+
+    np_top_labs = top_labs[0].numpy()
+
+    top_labels = []
+    for label in np_top_labs:
+        top_labels.append(int(idx_to_class[label]))
+
+    top_flowers = [cat_to_name[str(lab)] for lab in top_labels]
+    print(top_flowers)
+    print(top_probs)
+    print(top_labels)
+    print(topk)
+    
+    #printing out the top K classes along with associated probabilities
+    i=0 # this prints out top k classes and probs as according to user 
+    while i < len(top_flowers):
+        print("{} with a probability of {}".format(top_flowers[i], top_probs[i]))
+        i += 1 # cycle through
+    
+    return top_probs, top_labels, top_flowers
+###############################
+    
+if __name__ == "__main__":
+    main()
+        
---
+++ 6. Neural Networks/2. Image Classifier Part 2/1. Work In Progress/train.py
@@ -0,0 +1,220 @@
+# PROGRAMMER: Steve M 
+# DATE CREATED: 11.07.20                                 
+# REVISED DATE: 
+# PURPOSE: Train a new image classifier network to recognize different species of flowers
+#          and save the model as a checkpoint
+
+# Import necessary libraries
+import matplotlib.pyplot as plt
+import torch
+import time
+import numpy as np
+from torchvision import datasets, transforms, models
+from torch import nn
+from torch import optim
+from collections import OrderedDict
+import torch.nn.functional as F
+import torchvision.models as models
+from torch.autograd import Variable
+from PIL import Image
+import seaborn as sns
+import argparse
+import json
+
+# Imports functions created for this program
+from get_input_args_train import get_input_args
+
+def main():
+
+    # Function to retrieve command line arguments entered by the user
+    in_arg = get_input_args()
+    
+    # Function that checks command line arguments using in_arg  
+    #check_command_line_arguments(in_arg)
+    
+    ## Load data
+    image_datasets, dataloaders, cat_to_name = load_data(in_arg.data_dir)
+        
+    # Load model
+    model = load_model(in_arg.arch, in_arg.hidden_units, in_arg.learning_rate)
+
+    # need better place to store this code
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.classifier.parameters(), lr=in_arg.learning_rate)
+    
+    # Train the model
+    train(model, in_arg.epochs, in_arg.learning_rate, criterion, optimizer, dataloaders['training'])
+#    train(model, in_arg.epochs, in_arg.learning_rate, criterion, optimizer, train_loader, validation_loader)
+
+    
+    # Save checkpoint
+    save_checkpoint(in_arg.save_dir, model, optimizer, in_arg.epochs, in_arg.arch, image_datasets)
+
+ #################################
+## Load transformed data set
+def load_data(data_dir):
+    """Load data"""
+
+    train_dir = data_dir + '/train'
+    valid_dir = data_dir + '/valid'
+    test_dir = data_dir + '/test'
+
+    # Define transforms for the training, validation, and testing sets    
+#    for directory in [train_dir, valid_dir, test_dir]:
+#        if not os.path.isdir(directory):
+#            raise IOError("Directory " + directory + " does not exist")
+    
+    # Define transforms for the training, validation, and testing sets
+    data_transforms = {
+        'training': transforms.Compose([
+            transforms.RandomRotation(30),
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+       ]),
+        'validation': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'testing': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
+    
+    #Load the datasets with ImageFolder
+    image_datasets = {
+        'training' : datasets.ImageFolder(train_dir, transform=data_transforms['training']),
+        'validation' : datasets.ImageFolder(valid_dir, transform=data_transforms['validation']),
+        'testing' : datasets.ImageFolder(test_dir, transform=data_transforms['testing'])
+    }
+
+    # Using the image datasets and the trainforms, define the dataloaders
+    dataloaders = {    
+        'training' : torch.utils.data.DataLoader(image_datasets['training'], batch_size=64, shuffle=True),
+        'validation' : torch.utils.data.DataLoader(image_datasets['validation'], batch_size=32, shuffle=True),
+        'testing' : torch.utils.data.DataLoader(image_datasets['testing'], batch_size=32, shuffle=False)
+    }
+    
+    # Save mapping of classes to indices from the training datasets
+    #class_to_idx = image_datasets['training'].class_to_idx #return class ID's present in training dataset
+    #print('Steve22')
+    #print(class_to_idx)
+    
+    ##load in a mapping from category label to category name
+    with open('cat_to_name.json', 'r') as f:
+        cat_to_name = json.load(f)
+    
+    return image_datasets, dataloaders, cat_to_name
+
+###########################  
+## Load model
+def load_model (arch, hidden_units, learning_rate):
+#    if arch == 'vgg19': #setting model based on vgg19
+    model = models.vgg19(pretrained=True)
+
+        #don't update weights of the pre-trained model
+    for param in model.parameters():
+        param.requires_grad = False 
+
+        # update classifier
+    classifier = nn.Sequential(OrderedDict([
+        ('fc1', nn.Linear(25088, hidden_units,  bias=True)), #first layer
+        ('Relu1', nn.ReLU()), #apply activation function
+        ('Dropout1', nn.Dropout(p = 0.3)),
+        ('fc2', nn.Linear(hidden_units, 102, bias=True)), #output layer
+        ('output', nn.LogSoftmax(dim=1)) #apply loss function
+    ]))  
+      
+    # replace classifier in pre-trained model with the updated version
+    model.classifier = classifier
+        
+    # Set criterion and optimizer
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.classifier.parameters(), lr=learning_rate)           
+    
+    print(criterion)
+    return model
+###########################
+##Train Model
+def train(model, epochs, learning_rate, criterion, optimizer, train_loader):
+
+    model.train() # Puts model into training mode
+    print_every = 40
+    steps = 0
+    use_gpu = False
+    start = time.time()
+    
+    # Check to see if GPU is available
+    if torch.cuda.is_available():
+        use_gpu = True
+        model.cuda()
+
+    else:
+        model.cpu()
+        
+    print('Model: ', model)
+    
+    # Iterate through each training pass based on number of epochs & GPU availability
+    for epoch in range(epochs):
+        running_loss = 0
+        for images, labels in iter(train_loader):
+            steps += 1
+
+            if use_gpu:
+                images = Variable(images.float().cuda())
+                labels = Variable(labels.long().cuda()) 
+            else:
+                images = Variable(images)
+                labels = Variable(labels) 
+
+            optimizer.zero_grad() # Clear the gradients to ensure they aren't accumulated
+     
+            # Forward and backward passes
+            output = model.forward(images) # Forward propogation 
+            loss = criterion(output, labels) # Calculates loss
+            loss.backward() # Calculates gradient
+            optimizer.step() # Updates weights based on gradient & learning rate
+
+            running_loss += loss.item()
+
+# To delete - no need to validate model as already done in pytorch version
+#            if steps % print_every == 0:
+#                validation_loss, accuracy = validate(model, criterion, dataloaders['validation']) # Call validate function
+#
+#                 print("Epoch: {}/{} ".format(epoch+1, epochs),
+#                        "Training Loss: {:.3f} ".format(running_loss/print_every),
+#                        "Validation Loss: {:.3f} ".format(validation_loss),
+#                        "Validation Accuracy: {:.3f}".format(accuracy))
+                
+    # Confirm time to train model
+    print(f"Time to train model: {(time.time() - start)/3:.3f} seconds")
+
+##############################################   
+def save_checkpoint(save_dir, model, optimizer, epochs, arch, image_datasets):
+    ## Save the checkpoint
+    model.cpu()
+
+    checkpoint = {
+        'epochs': epochs, #Number of epochs
+        'arch': model, #architecture used
+        'state_dict': model.state_dict(), #learned paramaters from training
+        'class_to_idx': image_datasets['training'].class_to_idx, #mapping of flower class values to flower indices
+        'optimizer_state_dict': optimizer.state_dict(),
+        'classifier': model.classifier} #Latest checkpoint to continue training later    
+                
+    torch.save(checkpoint, save_dir + '/checkpoint.pth')
+    print('Checkpoint saved!')
+                
+    return None
+
+###########################
+    
+if __name__ == "__main__":
+    main()
+    
