6d9993e5fd72f25d2d60801cbee2e132aae71434
moggst74 <60689573+moggst74@users.noreply.github.com>
2020-08-01 14:03:57 +1000

    Add files via upload

---
+++ 6. Neural Networks/2. Image Classifier Part 2/1. Work In Progress/Almost Done/predict (2).py
@@ -0,0 +1,113 @@
+# PROGRAMMER: Steve M 
+# DATE CREATED: 19.07.20                                 
+# REVISED DATE: 
+# PURPOSE: Load model checkpoint and rebuild model without having to retrain
+
+# Import necessary libraries
+import torch
+from torchvision import transforms, models
+#from PIL import Image
+import argparse
+import json
+
+# Imports functions created for this program
+from get_input_args_predict import get_input_args
+from utilities import process_image
+
+def main():
+
+    # Function to retrieve command line arguments entered by the user
+    in_arg = get_input_args()    
+    
+    #Load pre-trained model from checkpoint
+    model = load_checkpoint(in_arg.saved_checkpoint, in_arg.gpu)
+    #print(in_arg.saved_checkpoint)
+    #print(model)
+    
+    # Process Image
+    processed_image = process_image(in_arg.image_path) 
+    
+    # Classify Prediction
+    top_probs, top_labels, top_flowers = predict(in_arg.image_path, model, in_arg.category_names, in_arg.top_k)
+    
+###############################    
+def load_checkpoint(filepath, cpu_gpu):   
+    checkpoint = torch.load(filepath)
+    architecture = checkpoint['arch']
+    print('Architecture used: ', architecture)
+    
+    # validate the model used for training
+    if architecture =='vgg19':
+        model = models.vgg19(pretrained=True)
+    elif architecture =='alexnet':
+        model = models.alexnet(pretrained = True)
+    elif architecture == 'densenet121':
+        model = models.densenet121(pretrained=True)
+    else:
+        print("Wrong Model")
+
+    model.class_to_idx = checkpoint['class_to_idx']  #mapping of flower class values to flower indices
+    model.classifier = checkpoint['classifier']
+    model.load_state_dict(checkpoint['state_dict'])  #learned paramaters from training
+ 
+    # Check to see if GPU is available
+    if cpu_gpu == 'GPU':
+        if torch.cuda.is_available():
+            use_gpu = True
+            model.cuda() # use 'GPU' if user chooses GPU and it is available
+
+        else:
+            model.cpu() # use 'CPU' if user chooses GPU but GPU not available
+    else:
+        model.cpu() # use 'CPU' if user chooses not to use GPU
+
+    return model
+
+###############################
+
+def predict(image_path, model, category_names, topk):
+    #Predict the class (or classes) of an image using a trained deep learning model
+    
+    # Code to predict the class from an image file   
+    processed_image = process_image(image_path)
+    processed_image.unsqueeze_(0)
+    probs = torch.exp(model.forward(processed_image))
+    top_probs, top_labs = probs.topk(topk)
+
+    with open(category_names, 'r') as f:
+        cat_to_name = json.load(f)
+        
+    idx_to_class = {}
+    for key, value in model.class_to_idx.items():
+        idx_to_class[value] = key
+
+    np_top_labs = top_labs[0].numpy()
+
+    top_labels = []
+    for label in np_top_labs:
+        top_labels.append(int(idx_to_class[label]))
+
+    top_flowers = [cat_to_name[str(lab)] for lab in top_labels]
+    top_probs = top_probs[0].detach().numpy() #converts from tensor to nparray
+    #print(top_flowers)
+    #print(top_probs)
+    #print(top_labels)
+    #print(topk)
+    #print('top flowers length')
+    #print(len(top_flowers))
+    #print(len(top_probs))
+    
+    #printing out the top K classes along with associated probabilities
+    i=0 # this prints out top k classes and probs as according to user 
+    while i < topk:
+        print("{} with a probability of {}".format(top_flowers[i], top_probs[i]))
+        #print("{} with a probability of {}".format(top_flowers[i]))
+        #print('top_probs: ', top_probs[i])
+        i += 1 # cycle through
+    
+    return top_probs, top_labels, top_flowers
+###############################
+    
+if __name__ == "__main__":
+    main()
+        
---
+++ 6. Neural Networks/2. Image Classifier Part 2/1. Work In Progress/Almost Done/train (2).py
@@ -0,0 +1,200 @@
+# PROGRAMMER: Steve M 
+# DATE CREATED: 11.07.20                                 
+# REVISED DATE: 
+# PURPOSE: Train a new image classifier network to recognize different species of flowers
+#          and save the model as a checkpoint
+
+# Import necessary libraries
+import torch
+import time
+import numpy as np
+from torchvision import datasets, transforms, models
+from torch import nn
+from torch import optim
+from collections import OrderedDict
+import torch.nn.functional as F
+import torchvision.models as models
+from torch.autograd import Variable
+import argparse
+import json
+
+# Imports functions created for this program
+from get_input_args_train import get_input_args
+from utilities import load_data
+def main():
+
+    # Function to retrieve command line arguments entered by the user
+    in_arg = get_input_args()
+    
+    #start the timer for loading and training the model
+    start_time = time.time()
+    
+    ## Load data
+    image_datasets, dataloaders = load_data(in_arg.data_dir)
+    
+    # Get model
+    model = get_model(in_arg.arch)
+        
+    # Load model
+    model = load_model(model, in_arg.arch, in_arg.hidden_units, in_arg.learning_rate)
+
+    # Set criterion and optimizer parameters
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.classifier.parameters(), lr=in_arg.learning_rate)
+    
+    # Train and validate the model
+    train(model, in_arg.epochs, in_arg.learning_rate, criterion, optimizer, dataloaders['training'], dataloaders['validation'],in_arg.gpu, start_time)
+   
+    # Confirm time to train model
+    print(f"Time to train and validate model: {(time.time() - start_time):.3f} seconds")
+
+    # Save checkpoint
+    save_checkpoint(in_arg.save_dir, model, optimizer, in_arg.epochs, in_arg.arch, image_datasets, in_arg.learning_rate)
+
+###########################  
+## Get model
+def get_model(arch):
+    if arch == 'vgg19': #setting model based on vgg19
+        model = models.vgg19(pretrained=True)
+    elif arch == 'alexnet': #setting model based on alexnet
+        model = models.alexnet(pretrained = True)
+    elif arch == 'densenet121': #setting model based on densenet121
+        model = models.densenet121(pretrained = True)
+    return model
+
+#############################
+## Load model
+def load_model (model, arch, hidden_units, learning_rate):
+    # Hyperparameters for our network
+    if arch == 'vgg19': #setting model based on vgg19
+        input_size = 25088
+    elif arch == 'alexnet': #setting model based on alexnet
+        input_size = 9216
+    elif arch == 'densenet121':
+        input_size = 1024
+    output_size = 102
+
+    # update classifier
+    classifier = nn.Sequential(OrderedDict([
+        ('fc1', nn.Linear(input_size, hidden_units,  bias=True)), #first layer
+        ('Relu1', nn.ReLU()), #apply activation function
+        ('Dropout1', nn.Dropout(p = 0.3)),
+        ('fc2', nn.Linear(hidden_units, output_size, bias=True)), #output layer
+        ('output', nn.LogSoftmax(dim=1)) #apply loss function
+    ]))  
+      
+    #don't update weights of the pre-trained model
+    for param in model.parameters():
+        param.requires_grad = False 
+        
+    # replace classifier in pre-trained model with the updated version
+    model.classifier = classifier
+        
+    # Set criterion and optimizer
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.classifier.parameters(), lr=learning_rate)           
+    
+    #print(criterion)
+    return model
+
+###########################
+##Train Model
+def train(model, epochs, learning_rate, criterion, optimizer, train_loader, validate_loader, cpu_gpu, start_time):
+
+    model.train() # Puts model into training mode
+    print_every = 40
+    steps = 0
+    use_gpu = False
+    
+    # Check to see if GPU is available
+    if cpu_gpu == 'GPU':
+        if torch.cuda.is_available():
+            use_gpu = True
+            model.cuda() # use 'GPU' if user chooses GPU and it is available
+
+        else:
+            model.cpu() # use 'CPU' if user chooses GPU but GPU not available
+    else:
+        model.cpu() # use 'CPU' if user chooses not to use GPU
+        
+    #print('Model: ', model)
+    
+    # Iterate through each training pass based on number of epochs & GPU availability
+    for epoch in range(epochs):
+        running_loss = 0
+        for images, labels in iter(train_loader):
+            steps += 1
+
+            if use_gpu:
+                images = Variable(images.float().cuda())
+                labels = Variable(labels.long().cuda()) 
+            else:
+                images = Variable(images)
+                labels = Variable(labels) 
+
+            optimizer.zero_grad() # Clear the gradients to ensure they aren't accumulated
+     
+            # Forward and backward passes
+            output = model.forward(images) # Forward propogation 
+            loss = criterion(output, labels) # Calculates loss
+            loss.backward() # Calculates gradient
+            optimizer.step() # Updates weights based on gradient & learning rate
+
+            running_loss += loss.item()
+
+            # Validate the model
+            if steps % print_every == 0:
+                validation_loss, accuracy = validate(model, criterion, validate_loader) # Call validate function
+
+                print("Epoch: {}/{} ".format(epoch+1, epochs),
+                    "Training Loss: {:.3f} ".format(running_loss/print_every),
+                    "Validation Loss: {:.3f} ".format(validation_loss),
+                    "Validation Accuracy: {:.3f}".format(accuracy))
+                
+##############################################
+## Functionto validate the accuracy of the model
+def validate(model, criterion, data_loader):
+    model.eval() # Puts model into validation mode
+    accuracy = 0
+    test_loss = 0
+    
+    for inputs, labels in iter(data_loader):
+        if torch.cuda.is_available():
+            inputs = Variable(inputs.float().cuda(), volatile=True)
+            labels = Variable(labels.long().cuda(), volatile=True) 
+        else:
+            inputs = Variable(inputs, volatile=True)
+            labels = Variable(labels, volatile=True)
+
+        output = model.forward(inputs)
+        test_loss += criterion(output, labels).item()
+        ps = torch.exp(output).data 
+        equality = (labels.data == ps.max(1)[1])
+        accuracy += equality.type_as(torch.FloatTensor()).mean()
+
+    return test_loss/len(data_loader), accuracy/len(data_loader)
+
+#########
+def save_checkpoint(save_dir, model, optimizer, epochs, arch, image_datasets, learning_rate):
+    ## Save the checkpoint
+    model.cpu()
+
+    checkpoint = {
+        'epochs': epochs, #Number of epochs
+        'arch': arch, #architecture used
+        'state_dict': model.state_dict(), #learned paramaters from training
+        'class_to_idx': image_datasets['training'].class_to_idx, #mapping of flower class values to flower indices
+        'optimizer_state_dict': optimizer.state_dict(),
+        'learning_rate':learning_rate, # learning rate for model
+        'classifier': model.classifier} #Latest checkpoint to continue training later  
+                
+    torch.save(checkpoint, save_dir + '/checkpoint.pth')
+    print('Checkpoint saved!')
+                
+    return None
+
+###########################
+    
+if __name__ == "__main__":
+    main()
+    
